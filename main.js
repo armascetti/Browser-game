b9d04a5b4e4254caa0f44919bc921f3698de82ad
Mandy_Mascetti <armascetti@gmail.com>
2021-10-18 12:13:10 -0400

    toggle players turns

--- main.js
+++ main.js
@@ -4,49 +4,54 @@
 
 
 
-
-
 /*------------- Variables (state) -------------*/
 
- let isWinner, currentPlayer, gameSquare
-
+let isWinner, currentPlayer, gameSquare
 
 
 
 /*--------- Cached Element References ---------*/
 
-const gameGrid = document.getElementById("Grid")
-
+const gameGrid = document.querySelector("#Grid")
+//const resetBtn = document.querySelector("#reset-button")
+const messageEl = document.querySelector("#message")
 
 
 /*-------------- Event Listeners --------------*/
 
-
-
+//resetBtn.addEventListener('click', initializeBoard)
 
 
 /*----------------- Functions -----------------*/
-function initializeBoard(){
+function initializeBoard() {
   for (let index = 0; index < 42; index++) {
-  let gameSquare = document.createElement("div");
-  gameSquare.addEventListener('click', playGame)  
-  gameSquare.style.width = "100px";
-  gameSquare.style.height = "100px";
-  gameSquare.style.background = "white";
-  gameSquare.style.borderRadius = "50px"; 
-  gameGrid.appendChild(gameSquare)  
+    let gameSquare = document.createElement("div");
+    gameSquare.addEventListener('click', playGame)
+    gameSquare.style.width = "100px";
+    gameSquare.style.height = "100px";
+    gameSquare.style.background = "white";
+    gameSquare.style.borderRadius = "80px";
+    gameGrid.appendChild(gameSquare)
   }
-}
-
-function playGame(){
-  alert('Im clicked')
-  for(let i =0; i < gameSquare.length; i++){
-    gameSquare[i]
-    console.log(gameSquare[i])
-  }
+  currentPlayer = 1
 }
 
 
+
+function playGame() {
+  if (currentPlayer === 1) {
+    this.style.background = "red";
+    togglePlayer();
+  } else {
+    this.style.background = "yellow";
+    togglePlayer();
+  }
+}
+
+//helper function to toggle the players turns 
+function togglePlayer() {
+  currentPlayer = currentPlayer * -1
+}
 
 
 //run game below
