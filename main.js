9ccbb8bd0cd0c2fd9ddd528eb8f3557b5011a493
Mandy_Mascetti <armascetti@gmail.com>
2021-10-18 21:54:50 -0400

    added values to the board

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /*----------------- Constants -----------------*/
 
-gameArray = [
+let gameArray = [
+
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
   [0, 0, 0, 0, 0, 0, 0]
+
 ]
 
 /*------------- Variables (state) -------------*/
@@ -22,26 +24,34 @@ const gameGrid = document.querySelector("#Grid")
 const messageEl = document.querySelector("#message")
 
 
+
 /*-------------- Event Listeners --------------*/
 
 //resetBtn.addEventListener('click', initializeBoard)
 
-
 /*----------------- Functions -----------------*/
+
+//use a for loop to create smaller divs inside a large div
+//use another for loop to access the innerArray index 
+  // created a div, event listener, and gave it a className all in the function 
+// line 46 is to access the gameArray index and grabbing the nested arrays that inside the gameAray. setting the value of each div thats in the innerArray of the gameArray 
+//create and appending to the gameGrid 
+
 function initializeBoard() {
-  for (let index = 0; index < 42; index++){
-    let gameSquare = document.createElement("div");
+  for (let index = 0; index < gameArray.length; index++){
+   for (let innerArray = 0; innerArray < 7; innerArray++) {
+      let gameSquare = document.createElement("div");
     gameSquare.addEventListener('click', playGame)
-    gameSquare.style.width = "100px";
-    gameSquare.style.height = "100px";
-    gameSquare.style.background = "white";
-    gameSquare.style.borderRadius = "80px";
-    gameGrid.appendChild(gameSquare)
-  }
+    gameSquare.className = "square"  
+    gameSquare.value = gameArray[index][innerArray]
+    gameGrid.appendChild(gameSquare)      
+    }   
+  } 
   currentPlayer = 1
 }
 
 function playGame() {
+  console.log(this.value)
   if (currentPlayer === 1) {
     this.style.background = "red";
     togglePlayer();
